6ad8616cb6080683b8af04121364d149463865d5
Wellington Della Mura <della.mura@gmail.com>
2018-11-28 09:13:04 -0200

    Roteiro do trabalho final

---
+++ parsers/antlr2018/Linguagenzinha/src/linguagenzinha/Test.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package linguagenzinha;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author wellington
+ */
+public class Test {
+
+    private int xises(int y) {
+        if (y == 10) {
+            return y;
+        } else {
+            return y;
+        }
+
+    }
+
+    public static void main(String[] args) {
+
+        ArrayList<String> lst = new ArrayList<>();
+        lst.stream().parallel().forEach(i -> {
+            System.out.println(i);
+        });
+    }
+}
